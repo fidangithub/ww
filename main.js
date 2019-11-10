f7c5dff030aa18273b7fef7d6964da920d2c2de1
fidangithub <57586245+fidangithub@users.noreply.github.com>
2019-11-10 13:39:46 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/*const search = document.querySelector('.button__search');
+const header = document.querySelector('.header');
+const headerContent = document.querySelector('.header__content');
+const headerHamburger = document.querySelector('.header__hamburger');
+const headerMission = document.querySelector('.header__mission');
+const logo = document.querySelector('.header__logo');
+search.addEventListener('click', () => {
+  //headerContent.style.display = "none";
+  headerHamburger.style.display = 'flex';
+  headerMission.style.display = 'none';
+  search.style.display = 'none';
+});
+*/
